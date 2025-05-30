d84f6ef69629930869d3daab13fb27a6d02f695d
edx3mes <149536188+edx3mes@users.noreply.github.com>
2025-05-30 15:58:48 -0400

    create new manifest files

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Apis integration with vendor magnament",
+    'summary': "API for extracting screen printing products",
+    'author': "ng-ballet-world",
+    'website': "https://ng-balletworld.odoo.com/",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'product'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
